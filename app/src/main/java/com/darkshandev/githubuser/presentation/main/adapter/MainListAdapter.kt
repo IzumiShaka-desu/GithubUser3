3b152a34cb367e94be1a7fa746b94f0dfb45011a
IzumiShaka-desu <shakaaji29@gmail.com>
2022-02-23 13:18:43 +0700

    update adapter

--- app/src/main/java/com/darkshandev/githubuser/presentation/main/adapter/MainListAdapter.kt
+++ app/src/main/java/com/darkshandev/githubuser/presentation/main/adapter/MainListAdapter.kt
@@ -4,24 +4,23 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
-import com.darkshandev.githubuser.R
 import com.darkshandev.githubuser.data.models.ProfileUser
 import com.darkshandev.githubuser.databinding.ItemListBinding
 
-class MainListAdapter: RecyclerView.Adapter<MainListAdapter.ViewHolder>() {
+class MainListAdapter(private val listener: Listener) :
+    RecyclerView.Adapter<MainListAdapter.ViewHolder>() {
 
-    var userList = listOf<ProfileUser>()
+    var userList = ArrayList<ProfileUser>()
 
-    fun updateUserList(newList: List<ProfileUser>){
-        this.userList = newList
+    fun updateUserList(newList: List<ProfileUser>) {
+        this.userList.addAll(newList)
 
         notifyDataSetChanged()
-        Log.d("kokkkk",this.userList.size.toString())
+        Log.d("kokkkk", this.userList.size.toString())
     }
 
-    inner class ViewHolder(val binding: ItemListBinding): RecyclerView.ViewHolder(binding.root)
+    inner class ViewHolder(val binding: ItemListBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
         ViewHolder(
@@ -29,20 +28,19 @@ class MainListAdapter: RecyclerView.Adapter<MainListAdapter.ViewHolder>() {
                 LayoutInflater.from(parent.context)
             )
         )
-//        val inflater =  LayoutInflater.from(parent.context)
-//        val binding = ItemListBinding.inflate(inflater,parent,false)
-//        return ViewHolder(binding)
-//
-//    }
+
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.binding.profile = userList[position]
-        Log.d("posisi",userList[position].name+position.toString())
+        holder.binding.root.setOnClickListener {
+            listener.onItemClickListener(it, userList[position])
+        }
+        Log.d("posisi", userList[position].name + position.toString())
     }
 
     override fun getItemCount(): Int = userList.size
 
-    interface  Listener{
+    interface Listener {
         fun onItemClickListener(view: View, user: ProfileUser)
     }
 }
