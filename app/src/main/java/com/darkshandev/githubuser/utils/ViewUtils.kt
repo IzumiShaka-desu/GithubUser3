01bcca513317046e815e7384b49336f913f556ac
IzumiShaka-desu <shakaaji29@gmail.com>
2022-02-23 13:19:27 +0700

    update view utils

--- app/src/main/java/com/darkshandev/githubuser/utils/ViewUtils.kt
+++ app/src/main/java/com/darkshandev/githubuser/utils/ViewUtils.kt
@@ -1,13 +1,17 @@
 package com.darkshandev.githubuser.utils
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.view.View
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 import com.darkshandev.githubuser.R
 
-fun ImageView.loadCircleImage(name: String){
-    val defaultOption= RequestOptions()
+
+fun ImageView.loadCircleImage(name: String) {
+    val defaultOption = RequestOptions()
         .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
         .error(R.drawable.ic_broken_image)
         .circleCrop()
@@ -17,7 +21,16 @@ fun ImageView.loadCircleImage(name: String){
             resources.getIdentifier(
                 name,
                 "drawable",
-                context.packageName)
-        ).into(this)
+                context.packageName
+            )
+        )
+        .into(this)
 }
 
+fun getBitmapFromView(view: View): Bitmap? {
+    var bitmap =
+        Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+    var canvas = Canvas(bitmap)
+    view.draw(canvas)
+    return bitmap
+}
