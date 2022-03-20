730769122eddf503a02b280e9acdd26aee046ecc
IzumiShaka-desu <shakaaji29@gmail.com>
2022-03-20 22:13:58 +0700

    fav fragment

--- app/src/main/java/com/darkshandev/githubuser/presentation/view/FavoriteFragment.kt
+++ app/src/main/java/com/darkshandev/githubuser/presentation/view/FavoriteFragment.kt
@@ -1,33 +1,41 @@
 package com.darkshandev.githubuser.presentation.view
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.darkshandev.githubuser.R
+import com.darkshandev.githubuser.data.models.UserSearch
+import com.darkshandev.githubuser.databinding.FragmentFavoriteBinding
+import com.darkshandev.githubuser.presentation.adapter.MainListAdapter
+import com.darkshandev.githubuser.presentation.viewmodel.MainViewmodel
+import com.darkshandev.githubuser.utils.toListModelSearch
+import kotlinx.coroutines.launch
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class FavoriteFragment : Fragment(), MainListAdapter.Listener {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FavoriteFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class FavoriteFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var binding: FragmentFavoriteBinding? = null
+    private val mainViewModel: MainViewmodel by activityViewModels()
+    private val mAdapter by lazy { MainListAdapter(this) }
+    private val navController: NavController by lazy {
+        Navigation.findNavController(activity = activity as Activity, R.id.viewContainer)
+    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    override fun onDestroy() {
+        binding = null
+        super.onDestroy()
     }
 
     override fun onCreateView(
@@ -35,26 +43,51 @@ class FavoriteFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FavoriteFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FavoriteFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        binding = FragmentFavoriteBinding.inflate(inflater, container, false)
+        binding?.apply {
+            rvFav.apply {
+                layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+                adapter = mAdapter
+            }
+            toolbarFav.apply {
+                setupWithNavController(
+                    navController = navController,
+                    configuration = AppBarConfiguration(
+                        setOf(
+                            R.id.searchFragment,
+                            R.id.detailFragment,
+                            R.id.favoriteFragment
+                        )
+                    )
+                )
+                setNavigationOnClickListener {
+                    activity?.onBackPressed()
                 }
             }
+            NavigationUI.setupWithNavController(toolbarFav, navController)
+            toolbarFav.setNavigationOnClickListener {
+                activity?.onBackPressed()
+            }
+
+        }
+        initObserver()
+        return binding?.root
     }
+
+    private fun initObserver() {
+        lifecycleScope.launch {
+            mainViewModel.favoriteUser
+                .flowWithLifecycle(lifecycle, Lifecycle.State.STARTED)
+                .collect { result ->
+                    mAdapter.updateUserList(result.toListModelSearch())
+                }
+        }
+    }
+
+    override fun onItemClickListener(view: View, user: UserSearch) {
+        mainViewModel.setSelectedUsername(user.login)
+        navController.navigate(R.id.navigate_to_detailfav)
+    }
+
+
 }
