770ff0964a4e4472bc5b4e56d65d8b9ee594dbc1
IzumiShaka-desu <shakaaji29@gmail.com>
2022-03-20 22:04:37 +0700

    update interceptor

--- app/src/main/java/com/darkshandev/githubuser/app/DI/NetworkModule.kt
+++ app/src/main/java/com/darkshandev/githubuser/app/DI/NetworkModule.kt
@@ -1,6 +1,8 @@
 package com.darkshandev.githubuser.app.DI
 
+import com.darkshandev.githubuser.BuildConfig
 import com.darkshandev.githubuser.app.Config
+import com.darkshandev.githubuser.data.network.AuthInterceptor
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -14,23 +16,26 @@ import retrofit2.converter.gson.GsonConverterFactory
 @Module
 object NetworkModule {
     @Provides
-    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor{
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
         val interceptor = HttpLoggingInterceptor()
         interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
         return interceptor
     }
+
     @Provides
     fun provideOkHTTPClient(
-        loggingInterceptor:HttpLoggingInterceptor
-    ):OkHttpClient=
+        loggingInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient =
         OkHttpClient
             .Builder()
             .addInterceptor(loggingInterceptor)
+            .addInterceptor(AuthInterceptor(BuildConfig.token))
             .build()
+
     @Provides
     fun provideRetrofit(
         okHttpClient: OkHttpClient
-    ):Retrofit=Retrofit
+    ): Retrofit = Retrofit
         .Builder()
         .baseUrl(Config.BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
