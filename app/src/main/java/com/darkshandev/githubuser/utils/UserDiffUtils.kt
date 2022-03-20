8ccd0155bf9a857ea6f13dbb680aa31e036aabbd
IzumiShaka-desu <shakaaji29@gmail.com>
2022-03-20 22:10:38 +0700

    diff utils

--- app/src/main/java/com/darkshandev/githubuser/utils/UserDiffUtils.kt
+++ app/src/main/java/com/darkshandev/githubuser/utils/UserDiffUtils.kt
@@ -1,4 +1,24 @@
 package com.darkshandev.githubuser.utils
 
-class UserDiffUtils {
+import androidx.recyclerview.widget.DiffUtil
+import com.darkshandev.githubuser.data.models.UserSearch
+
+class UserDiffUtils(private val oldList: List<UserSearch>, private val newList: List<UserSearch>) :
+    DiffUtil.Callback() {
+    override fun getOldListSize(): Int = oldList.size
+
+    override fun getNewListSize(): Int = newList.size
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+        oldList == newList
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val old = oldList[oldItemPosition]
+        val latest = newList[newItemPosition]
+        return when {
+            old.id == latest.id -> true
+            old.login == latest.login -> true
+            else -> false
+        }
+    }
 }
