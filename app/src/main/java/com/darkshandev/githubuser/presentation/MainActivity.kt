d0e61d83c601d8aee715585f4f187442ed88690a
IzumiShaka-desu <shakaaji29@gmail.com>
2022-03-20 22:09:59 +0700

    update mianactivity

--- app/src/main/java/com/darkshandev/githubuser/presentation/MainActivity.kt
+++ app/src/main/java/com/darkshandev/githubuser/presentation/MainActivity.kt
@@ -1,11 +1,16 @@
-package com.darkshandev.githubuser.presentation.main
+package com.darkshandev.githubuser.presentation
 
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import com.darkshandev.githubuser.databinding.ActivityMainBinding
-import com.darkshandev.githubuser.presentation.main.viewmodel.MainViewmodel
+import com.darkshandev.githubuser.presentation.viewmodel.MainViewmodel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -25,6 +30,22 @@ class MainActivity : AppCompatActivity() {
 
     private fun initActivity() {
         setContentView(binding.root)
+        lifecycleScope.launch {
+            mainViewmodel.themeSettings
+                .flowWithLifecycle(lifecycle, Lifecycle.State.STARTED)
+                .collect { result ->
+                    setThemeMode(if (result) 1 else 0)
+                }
+        }
+    }
+
+    private fun setThemeMode(theme: Int) {
+        val mode = when (theme) {
+            0 -> AppCompatDelegate.MODE_NIGHT_NO
+            1 -> AppCompatDelegate.MODE_NIGHT_YES
+            else -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+        }
+        AppCompatDelegate.setDefaultNightMode(mode)
     }
 
 
