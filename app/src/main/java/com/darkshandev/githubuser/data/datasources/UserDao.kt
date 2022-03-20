72c4ae80a23a50c7346f9f192bf46b402d55bda7
IzumiShaka-desu <shakaaji29@gmail.com>
2022-03-20 22:05:04 +0700

    create userdao

--- app/src/main/java/com/darkshandev/githubuser/data/datasources/UserDao.kt
+++ app/src/main/java/com/darkshandev/githubuser/data/datasources/UserDao.kt
@@ -1,4 +1,24 @@
 package com.darkshandev.githubuser.data.datasources
 
+import androidx.room.*
+import com.darkshandev.githubuser.data.models.UserEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface UserDao {
+    @Query("SELECT * FROM user_table")
+    fun getFavUser(): Flow<List<UserEntity>>
+
+    @Delete
+    suspend fun delete(user: UserEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addFavoriteUser(user: UserEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addAll(users: List<UserEntity>)
+
+    @Delete
+    suspend fun deleteAll(users: List<UserEntity>)
+
 }
