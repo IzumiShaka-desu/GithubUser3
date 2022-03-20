d13a0cbfc55db52823444c7505806fc8441352cc
IzumiShaka-desu <shakaaji29@gmail.com>
2022-03-20 22:29:29 +0700

    back to submission 2

--- app/src/main/java/com/darkshandev/githubuser/presentation/viewmodel/MainViewmodel.kt
+++ app/src/main/java/com/darkshandev/githubuser/presentation/viewmodel/MainViewmodel.kt
@@ -2,41 +2,22 @@ package com.darkshandev.githubuser.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.darkshandev.githubuser.data.datasources.SettingPreferences
-import com.darkshandev.githubuser.data.models.*
+import com.darkshandev.githubuser.data.models.Result
+import com.darkshandev.githubuser.data.models.SearchResponse
+import com.darkshandev.githubuser.data.models.UserDetail
+import com.darkshandev.githubuser.data.models.UserSearch
 import com.darkshandev.githubuser.data.repositories.GithubUserRepository
-import com.darkshandev.githubuser.utils.toEntity
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewmodel @Inject constructor(
-    private val repository: GithubUserRepository,
-    private val pref: SettingPreferences
+    private val repository: GithubUserRepository
 ) : ViewModel() {
-
-    val themeSettings: StateFlow<Boolean> = pref.getThemeSetting().stateIn(
-        viewModelScope,
-        SharingStarted.Lazily,
-        false
-    )
-
-
-    fun saveThemeSetting(isDarkModeActive: Boolean) {
-        viewModelScope.launch {
-            pref.saveThemeSetting(isDarkModeActive)
-        }
-    }
-
     fun setQuery(query: String) =
         repository.setQuery(query)
 
-    @FlowPreview
-    @ExperimentalCoroutinesApi
     val searchResult: StateFlow<Result<SearchResponse>> = repository
         .searchUser()
         .stateIn(
@@ -46,12 +27,10 @@ class MainViewmodel @Inject constructor(
         )
 
     private val _username = MutableStateFlow("")
-    val username: StateFlow<String> = _username
     fun setSelectedUsername(newUsername: String) {
         _username.value = newUsername
     }
 
-    @ExperimentalCoroutinesApi
     val detailUser: StateFlow<Result<UserDetail>> = _username
         .distinctUntilChanged { old, new -> old == new && new == "" }
         .transformLatest {
@@ -66,8 +45,6 @@ class MainViewmodel @Inject constructor(
             SharingStarted.Lazily,
             Result.loading()
         )
-
-    @ExperimentalCoroutinesApi
     val followerUser: StateFlow<Result<List<UserSearch>>> = _username
         .distinctUntilChanged { old, new -> old == new && new == "" }
         .transformLatest {
@@ -75,13 +52,13 @@ class MainViewmodel @Inject constructor(
                 .collect { result ->
                     emit(result)
                 }
+
         }.stateIn(
             viewModelScope,
             SharingStarted.Lazily,
             Result.loading()
         )
 
-    @ExperimentalCoroutinesApi
     val followingUser: StateFlow<Result<List<UserSearch>>> = _username
         .distinctUntilChanged { old, new -> old == new && new == "" }
         .transformLatest {
@@ -95,31 +72,4 @@ class MainViewmodel @Inject constructor(
             SharingStarted.Lazily,
             Result.loading()
         )
-
-    @ExperimentalCoroutinesApi
-    val favoriteUser: StateFlow<List<UserEntity>> = repository
-        .getFavoriteUser()
-        .stateIn(
-            viewModelScope,
-            SharingStarted.Lazily,
-            emptyList()
-        )
-
-
-    @ExperimentalCoroutinesApi
-    fun addOrRemoveFavorite() {
-        detailUser.value.data?.let { detail ->
-            viewModelScope.launch {
-                val isFavourited = favoriteUser.value.any { it.login == username.value }
-                if (isFavourited) {
-                    repository.removeFavorite(detail.toEntity())
-                } else {
-                    repository.addFavorite(detail.toEntity())
-                }
-
-            }
-
-        }
-
-    }
 }
