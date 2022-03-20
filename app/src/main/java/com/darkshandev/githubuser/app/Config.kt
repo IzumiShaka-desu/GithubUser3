9144c1bd48e7bcc9de7949043efd520d93daa026
IzumiShaka-desu <shakaaji29@gmail.com>
2022-03-20 22:05:11 +0700

    update config

---
+++ app/src/main/java/com/darkshandev/githubuser/app/Config.kt
@@ -0,0 +1,7 @@
+package com.darkshandev.githubuser.app
+
+object Config {
+    const val DB_Name = "githubApp.db"
+    const val BASE_URL = "https://api.github.com"
+    const val USER_TABLE = "user_table"
+}
