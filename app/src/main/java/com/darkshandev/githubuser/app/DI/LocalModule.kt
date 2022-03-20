6df3e6043c61b3527b24fc2d29f2ee81c3ec739e
IzumiShaka-desu <shakaaji29@gmail.com>
2022-03-20 22:04:20 +0700

    add local module injector

--- app/src/main/java/com/darkshandev/githubuser/app/DI/LocalModule.kt
+++ app/src/main/java/com/darkshandev/githubuser/app/DI/LocalModule.kt
@@ -1,4 +1,33 @@
 package com.darkshandev.githubuser.app.DI
 
+import android.content.Context
+import androidx.room.Room
+import com.darkshandev.githubuser.app.Config
+import com.darkshandev.githubuser.data.database.AppDatabase
+import com.darkshandev.githubuser.data.datasources.UserDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
 object LocalModule {
+    @Provides
+    @Singleton
+    fun provideAppDatabase(
+        @ApplicationContext appContext: Context
+    ): AppDatabase = Room
+        .databaseBuilder(
+            appContext,
+            AppDatabase::class.java,
+            Config.DB_Name
+        ).build()
+
+    @Provides
+    fun provideUserDao(appDatabase: AppDatabase): UserDao {
+        return appDatabase.userDao()
+    }
 }
