c61ce7f12824651d4846b01ecc06f4f20f666985
IzumiShaka-desu <shakaaji29@gmail.com>
2022-03-20 22:10:47 +0700

    view utils

--- app/src/main/java/com/darkshandev/githubuser/utils/ViewUtils.kt
+++ app/src/main/java/com/darkshandev/githubuser/utils/ViewUtils.kt
@@ -2,35 +2,44 @@ package com.darkshandev.githubuser.utils
 
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.drawable.ColorDrawable
 import android.view.View
 import android.widget.ImageView
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 import com.darkshandev.githubuser.R
 
 
-fun ImageView.loadCircleImage(name: String) {
+fun ImageView.loadCircleImage(url: String) {
     val defaultOption = RequestOptions()
         .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
         .error(R.drawable.ic_broken_image)
         .circleCrop()
-    Glide.with(context)
-        .setDefaultRequestOptions(defaultOption)
-        .load(
-            resources.getIdentifier(
-                name,
-                "drawable",
-                context.packageName
+    if (url.isEmpty()) {
+        Glide.with(context)
+            .setDefaultRequestOptions(defaultOption)
+            .load(
+                ColorDrawable(ContextCompat.getColor(context, R.color.grey))
             )
-        )
-        .into(this)
+            .into(this)
+    } else {
+
+
+        Glide.with(context)
+            .setDefaultRequestOptions(defaultOption)
+            .load(
+                url
+            )
+            .into(this)
+    }
 }
 
 fun getBitmapFromView(view: View): Bitmap? {
-    var bitmap =
+    val bitmap =
         Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
-    var canvas = Canvas(bitmap)
+    val canvas = Canvas(bitmap)
     view.draw(canvas)
     return bitmap
 }
