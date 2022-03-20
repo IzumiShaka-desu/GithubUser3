a80894c78d92466608f4173420f7414d3aa59ed8
IzumiShaka-desu <shakaaji29@gmail.com>
2022-03-20 22:10:25 +0700

    user entity

--- app/src/main/java/com/darkshandev/githubuser/utils/UserEntityDiffUtils.kt
+++ app/src/main/java/com/darkshandev/githubuser/utils/UserEntityDiffUtils.kt
@@ -1,4 +1,27 @@
 package com.darkshandev.githubuser.utils
 
-class UserEntityDiffUtils {
+import androidx.recyclerview.widget.DiffUtil
+import com.darkshandev.githubuser.data.models.UserEntity
+
+class UserEntityDiffUtils(
+    private val oldList: List<UserEntity>,
+    private val newList: List<UserEntity>
+) :
+    DiffUtil.Callback() {
+    override fun getOldListSize(): Int = oldList.size
+
+    override fun getNewListSize(): Int = newList.size
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+        oldList == newList
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val old = oldList[oldItemPosition]
+        val latest = newList[newItemPosition]
+        return when {
+            old.id == latest.id -> true
+            old.login == latest.login -> true
+            else -> false
+        }
+    }
 }
