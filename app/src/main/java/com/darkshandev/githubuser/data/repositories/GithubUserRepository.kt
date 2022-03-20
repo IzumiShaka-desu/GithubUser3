c2603ca4b5f0888cec26807c4a810dc72a8e5d3f
IzumiShaka-desu <shakaaji29@gmail.com>
2022-03-20 22:25:23 +0700

    back to submission one

--- app/src/main/java/com/darkshandev/githubuser/data/repositories/GithubUserRepository.kt
+++ app/src/main/java/com/darkshandev/githubuser/data/repositories/GithubUserRepository.kt
@@ -1,87 +1,22 @@
 package com.darkshandev.githubuser.data.repositories
 
-import androidx.annotation.WorkerThread
-import com.darkshandev.githubuser.data.datasources.RemoteDataSource
-import com.darkshandev.githubuser.data.datasources.UserDao
-import com.darkshandev.githubuser.data.models.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.withContext
-import javax.inject.Inject
+import com.darkshandev.githubuser.data.models.ProfileUser
+import com.google.gson.GsonBuilder
 
-class GithubUserRepository @Inject constructor(
-    private val remoteDataSource: RemoteDataSource,
-    private val userDao: UserDao
-) {
+class GithubUserRepository {
 
-    private val _query = MutableStateFlow("")
-    fun setQuery(query: String) {
-        _query.value = query
+    fun getGithubUserListFromJson(jsonString: String): List<ProfileUser> {
+        return GsonBuilder().create().fromJson(jsonString, Array<ProfileUser>::class.java).toList()
     }
 
-    @ExperimentalCoroutinesApi
-    @FlowPreview
-    fun searchUser(): Flow<Result<SearchResponse>> = _query.debounce(300)
-        .flatMapLatest { query ->
-            flow {
-                if (query.isEmpty()) {
-                    emit(
-                        Result.error(
-                            message = "type something a discover new github friends",
-                            Error()
-                        )
-                    )
-                } else {
-                    emit(remoteDataSource.searchUserBy(query))
-                }
-            }.onStart {
-                emit(Result.loading())
-            }.catch {
-                emit(
-                    Result.error(
-                        message = "Sorry, there are no pups by that name. Keep looking!",
-                        Error()
-                    )
-                )
+    companion object {
+        var service: GithubUserRepository? = null
+
+        fun getInstance(): GithubUserRepository {
+            if (service == null) {
+                service = GithubUserRepository()
             }
-        }
-        .flowOn(Dispatchers.IO)
-
-    suspend fun getDetailUserBy(username: String): Flow<Result<UserDetail>> = flow {
-        emit(Result.loading())
-        val result = remoteDataSource.getDetailUserBy(username)
-        emit(result)
-    }.flowOn(Dispatchers.IO)
-
-    suspend fun getFollowingUserBy(username: String): Flow<Result<List<UserSearch>>> = flow {
-        emit(Result.loading())
-        val result = remoteDataSource.getFollowingUserBy(username)
-        emit(result)
-    }.flowOn(Dispatchers.IO)
-
-    suspend fun getFollowerUserBy(username: String): Flow<Result<List<UserSearch>>> = flow {
-        emit(Result.loading())
-        val result = remoteDataSource.getFollowerUserBy(username)
-        emit(result)
-    }.flowOn(Dispatchers.IO)
-
-    @Suppress("RedundantSuspendModifier")
-    @WorkerThread
-    suspend fun addFavorite(user: UserEntity) {
-        withContext(Dispatchers.IO) {
-            userDao.addFavoriteUser(user)
+            return service!!
         }
     }
-
-    @Suppress("RedundantSuspendModifier")
-    @WorkerThread
-    suspend fun removeFavorite(user: UserEntity) {
-        withContext(Dispatchers.IO) {
-            userDao.delete(user)
-        }
-    }
-
-    fun getFavoriteUser() = userDao.getFavUser()
 }
