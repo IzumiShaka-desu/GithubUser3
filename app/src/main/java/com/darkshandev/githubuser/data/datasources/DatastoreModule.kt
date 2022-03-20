52e898b4de4c3dc6ea9cc958e15c16d170f43b6c
IzumiShaka-desu <shakaaji29@gmail.com>
2022-03-20 22:14:25 +0700

    datastoremodule injector

--- app/src/main/java/com/darkshandev/githubuser/data/datasources/DatastoreModule.kt
+++ app/src/main/java/com/darkshandev/githubuser/data/datasources/DatastoreModule.kt
@@ -1,4 +1,32 @@
 package com.darkshandev.githubuser.data.datasources
 
-class DatastoreModule {
+import android.content.Context
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.preferencesDataStore
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+private val Context.dataStore by preferencesDataStore("settings")
+
+class SettingPreferences @Inject constructor(@ApplicationContext appContext: Context) {
+
+    private val dataStore = appContext.dataStore
+    private val THEME_KEY = booleanPreferencesKey("theme_setting")
+
+    fun getThemeSetting(): Flow<Boolean> {
+        return dataStore.data.map { preferences ->
+            preferences[THEME_KEY] ?: false
+        }
+    }
+
+    suspend fun saveThemeSetting(isDarkModeActive: Boolean) {
+        dataStore.edit { preferences ->
+            preferences[THEME_KEY] = isDarkModeActive
+        }
+    }
+
+
 }
