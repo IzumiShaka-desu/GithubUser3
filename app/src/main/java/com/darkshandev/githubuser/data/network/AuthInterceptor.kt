104d4281fb42e666e9525efa176d0c187c3cafef
IzumiShaka-desu <shakaaji29@gmail.com>
2022-03-20 22:06:43 +0700

    add auth interceptor

--- app/src/main/java/com/darkshandev/githubuser/data/network/AuthInterceptor.kt
+++ app/src/main/java/com/darkshandev/githubuser/data/network/AuthInterceptor.kt
@@ -1,4 +1,16 @@
 package com.darkshandev.githubuser.data.network
 
-class AuthInterceptor {
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AuthInterceptor(private var token: String) : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val req = chain.request()
+        val basicReq = req.newBuilder()
+            .addHeader("Authorization", "token $token")
+            .build()
+
+        return chain.proceed(basicReq)
+    }
 }
